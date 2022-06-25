a32f81e3cc5755be88791238599ed40ec4c4a888
athulpranav <athulpranav01@gmail.com>
2022-06-25 02:39:01 -0700

    done

---
+++ index.js
@@ -0,0 +1,41 @@
+loginbtn=()=>{
+   USERNAME=(document.getElementById("usrname").value)
+    PASSWORD=(document.getElementById("password").value)
+   
+     console.log(USERNAME)
+     alert(USERNAME)
+
+     console.log(PASSWORD)
+     alert(PASSWORD)
+}
+regbtn=()=>{
+    NAME=(document.getElementById("name").value)
+    ADDRESS=(document.getElementById("address").value)
+    MOBILE=(document.getElementById("mobile").value)
+    EMAILID=(document.getElementById("email").value)
+    BLOOD=(document.getElementById("bld").value)
+    COLLEGE=(document.getElementById("clg").value)
+    USERNAME=(document.getElementById("usrname").value)
+    PASSWORD=(document.getElementById("password").value)
+     console.log(NAME)
+     alert(NAME)
+
+     console.log(ADDRESS)
+     alert(ADDRESS)
+
+     console.log(MOBILE)
+     alert(MOBILE)
+
+     console.log(EMAILID)
+     alert(EMAILID)
+
+     console.log(BLOOD)
+     alert(BLOOD)
+     console.log(COLLEGE)
+     alert(COLLEGE)
+     console.log(USERNAME)
+     alert(USERNAME)
+     console.log(PASSWORD)
+     alert(PASSWORD)
+}
+
